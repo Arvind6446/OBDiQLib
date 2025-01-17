c868178e2451d5e779e69747c56642ebf6435f27
Arvind_Cardr <arvind@cardr.com>
2025-01-17 22:54:54 +0530

    latest

---
+++ OBDiQLib.xcframework/ios-arm64/OBDiQLib.framework/Headers/OBDiQLib.h
@@ -0,0 +1,18 @@
+//
+//  OBDiQLib.h
+//  OBDiQLib
+//
+//  Created by Arvind Mehta on 17/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OBDiQLib.
+FOUNDATION_EXPORT double OBDiQLibVersionNumber;
+
+//! Project version string for OBDiQLib.
+FOUNDATION_EXPORT const unsigned char OBDiQLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OBDiQLib/PublicHeader.h>
+
+
